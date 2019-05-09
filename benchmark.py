7818837387cb92935b16b3791600369ddbc7da67
huongnhd <huongnhd@lifull-tech.vn>
2019-05-09 17:13:40 +0700

    adding mongo db

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,66 @@
-import psycopg2
-import MySQLdb
 import time
 import logging
+import bson
+import os
+import json
+import enum
+import pymongo
+
+import psycopg2
+import MySQLdb
+from pymongo import MongoClient
+
+from bson import json_util
+from enum import Enum
+
+
 logger = logging.getLogger(__name__)
 
 
-def current_milli_time(): return time.time() * 1000
+class DBMS(Enum):
+    MARIADB = 'mariadb'
+    MONGODB = 'mongodb'
+    POSTGRESQL = 'postgres'
+    MYSQL = 'mysql'
+
+
+class STORE_TYPE(Enum):
+    NOSQL = 'nosql'
+    SQL = 'sql'
+
+
+CONNECTION_INIT = {
+    DBMS.POSTGRESQL.value: {
+        'host': '127.0.0.1',
+        'user': 'postgres',
+        'passwd': 'password',
+        'port': 5433
+    },
+    DBMS.MYSQL.value: {
+        'host': '127.0.0.1',
+        'user': 'root',
+        'passwd': 'password',
+        'port': 3302
+    },
+    DBMS.MARIADB.value: {
+        'host': '127.0.0.1',
+        'user': 'root',
+        'passwd': 'password',
+        'port': 3307
+    },
+    DBMS.MONGODB.value: {
+        'host': '127.0.0.1',
+        'port': 27018
+    }
+}
+
+
+def current_mills_time(): return time.time() * 1000
 
 
 class Benchmark:
-    def __init__(self, dbtype, arr_dict):
+    def __init__(self, db_type, arr_dict, write_dir='benchmark', db='benchmark'):
+        self.dbms = db_type  # database management system
         self.time_unit = 'ms'
         self.read_query = {
             "q1": "SELECT * FROM testing LIMIT 1000",
@@ -19,37 +70,39 @@ class Benchmark:
             "q5": "SELECT * FROM testing WHERE int_col > 5000 ORDER BY word_col ASC LIMIT 1000",
             "q6": "SELECT * FROM testing WHERE word_col LIKE '%lim%' ORDER BY word_col DESC LIMIT 1000"
         }
-        self.query = {}
-        if dbtype == 'mariadb':
-            self.pre = 'ma'
-            self.db_initfile = 'mysql.sql'
-            self.query['truncate'] = 'TRUNCATE TABLE testing'
-            self.query['config'] = 'show variables;'
-        elif dbtype == 'mysql':
-            self.pre = 'my'
-            self.db_initfile = 'mysql.sql'
-            self.query['truncate'] = 'TRUNCATE TABLE testing'
-            self.query['config'] = 'show variables;'
-        elif dbtype == 'postgres':
-            self.pre = 'pg'
+        # self.read_query_nosql = {
+        #     "q1": "{}",
+        # }
+        self.db_mode = STORE_TYPE.SQL  # include sql and no-sql
+        if self.dbms == DBMS.POSTGRESQL:
             self.db_initfile = 'postgres.sql'
             self.query['truncate'] = 'TRUNCATE TABLE testing RESTART IDENTITY'
             self.query['config'] = 'show all;'
+        elif self.dbms == DBMS.MONGODB:
+            self.db_mode = STORE_TYPE.NOSQL
+        else:  # mysql or mariadb`
+            self.db_initfile = 'mysql.sql'
+            self.query['truncate'] = 'TRUNCATE TABLE testing'
+            self.query['config'] = 'show variables;'
 
-        self.host = arr_dict['host']
-        self.user = arr_dict['user']
-        self.passwd = arr_dict['passwd']
-        self.port = arr_dict['port']
-        self.db = 'benchmark'
-        self.write_dir = 'benchmark'
+        self.write_dir = write_dir
+        # get config of connection
+        self.host = arr_dict.get('host', '127.0.0.1')
+        self.user = arr_dict.get('user', None)
+        self.passwd = arr_dict.get('passwd', None)
+        self.port = arr_dict.get('port', None)
+        self.db = db
+        self.init()
 
     def init(self):
-        if self.pre == 'pg':
+        if self.dbms == DBMS.POSTGRESQL:
             self.pg_init()
-        elif self.pre == 'my':
+        elif self.dbms == DBMS.MYSQL:
             self.my_init()
-        elif self.pre == 'ma':
+        elif self.dbms == DBMS.MARIADB:
             self.my_init()
+        elif self.dbms == DBMS.MONGODB:
+            self.mo_init()
 
     def my_init(self):
         mc = self._connect()
@@ -59,6 +112,13 @@ class Benchmark:
         r.execute(data)
         r.close()
         print("mysql benchmark table + DB init successfully")
+
+    def mo_init(self):
+        client = self._connect()
+        db = client['benchmark']  # create database
+        db.testing  # create collation
+        print("mongodb benchmark DB:{},collection:{} init successfully".format(
+            'benchmark', 'testing'))
 
     def ma_init(self):
         mc = self._connect()
@@ -87,136 +147,207 @@ class Benchmark:
         print("postgres benchmark table + DB successfully")
 
     def truncate(self):
-        conn = self._connect_db()
-        r = conn.cursor()
-        r.execute(self.query['truncate'])
-        r.close()
-        conn.commit()
-        conn.close()
-
-    def write(self):
-        self.truncate()
-        with open('sql/insert.sql', 'r') as myfile:
-            query = myfile.read().replace('\n', '')
-        iter = 100
-        wf = open(self.write_dir + '/' + self.pre + '_write.txt', 'wb')
-        for x in range(0, iter):
+        if self.db_mode == STORE_TYPE.SQL:
             conn = self._connect_db()
             r = conn.cursor()
-            t1 = current_milli_time()
-            r.execute(query)
-            t2 = current_milli_time()
-            time_used = t2 - t1
-            wf.write(str(time_used) + '\n')
+            r.execute(self.query['truncate'])
             r.close()
             conn.commit()
             conn.close()
-        wf.close()
+        else:
+            db = self._connect_db()
+            print(db['testing'].drop())
+
+    def write(self):
+        self.truncate()
+        if self.db_mode == STORE_TYPE.SQL:
+            self._sql_write()
+        else:
+            self._no_sql_write()
+
+    def _no_sql_write(self):
+        data = []
+        with open('nosql/data.json', 'r') as data_insert_file:
+            data = json.load(data_insert_file)
+            iter = 100
+            _write_file_report = os.path.join(
+                self.write_dir,  self.dbms.value + '_write.txt')
+            with open(_write_file_report, 'wb') as wf:
+                for x in range(0, iter):
+                    self.truncate()
+                    conn = self._connect_db()
+                    coll = conn.testing
+                    print(coll.count())
+                    t1 = current_mills_time()
+                    print(coll.insert_many(data))
+                    t2 = current_mills_time()
+                    time_used = t2 - t1
+                    wf.write(str(time_used) + '\n')
+
+    def _sql_write(self):
+        with open('sql/insert.sql', 'r') as myfile:
+            query = myfile.read().replace('\n', '')
+            iter = 100
+            _write_file_report = os.path.join(
+                self.write_dir,  self.dbms.value + '_write.txt')
+            with open(_write_file_report, 'wb') as wf:
+                for x in range(0, iter):
+                    conn = self._connect_db()
+                    r = conn.cursor()
+                    t1 = current_mills_time()
+                    r.execute(query)
+                    t2 = current_mills_time()
+                    time_used = t2 - t1
+                    wf.write(str(time_used) + '\n')
+                    r.close()
+                    conn.commit()
+                    conn.close()
 
     def read(self):
-        for key in self.read_query:
-            self._query(self.read_query[key], key)
+        if self.db_mode == STORE_TYPE.SQL:
+            for key in self.read_query:
+                self._query(self.read_query[key], key)
+        else:
+            # pass
+            for key in self.read_query:
+                self._query_mongodb(key, key)
 
     def _query(self, query, filename):
         iter = 100
-        wf = open(self.write_dir + '/' + self.pre +
-                  '_' + filename + '.txt', 'wb')
-        for x in range(0, iter):
+        out_put_file = os.path.join(
+            self.write_dir,  self.dbms.value + '_' + filename + '.txt')
+        with open(out_put_file, 'wb') as wf:
+          for _ in range(0, iter):
             conn = self._connect_db()
-            r = conn.cursor()
-            t1 = current_milli_time()
-            r.execute(query)
-            t2 = current_milli_time()
+            coll = conn.testing
+            t1 = current_mills_time()
+            coll.execute(query)
+            t2 = current_mills_time()
             time_used = t2 - t1
             wf.write(str(time_used) + '\n')
-            r.close()
-            conn.close()
-        wf.close()
 
-    def config(self):
-        with open(self.write_dir + '/' + self.pre + '_config.txt', 'wb') as wf:
+    def _query_mongodb(self, query_key, filename):
+        iter = 100
+        out_put_file = os.path.join(
+            self.write_dir,  self.dbms.value + '_' + filename + '.txt')
+        with open(out_put_file, 'wb') as wf:
+          for x in range(0, iter):
             conn = self._connect_db()
-            r = conn.cursor()
-            r.execute(self.query['config'])
-            for row in r:
-                wf.write(str(row[0]) + ': ' + str(row[1]) + '\n')
-            r.close()
-            conn.close()
+            coll = conn.testing
+            if query_key == 'q1':
+                t1 = current_mills_time()
+                coll.find().limit(1000)
+                t2 = current_mills_time()
+                time_used = t2 - t1
+                wf.write(str(time_used) + '\n')
+            if query_key == 'q2':
+                t1 = current_mills_time()
+                #  "SELECT * FROM testing WHERE int_col > 5000 LIMIT 1000",
+                coll.find({'int_col': {"$gt": 5000} }).limit(1000)
+                t2 = current_mills_time()
+                time_used = t2 - t1
+                wf.write(str(time_used) + '\n')
+            if query_key == 'q3':
+                t1 = current_mills_time()
+                # "q3": "SELECT * FROM testing WHERE int_col + int_col2 > 12345 LIMIT 1000",
+                # coll.find({ $where: "/^.*test.*$/.test(this.int_col + this.int_col2)" } )).limit(1000)
+                coll.find({'int_col': {"$gt": 12345} }).limit(1000)
+                t2 = current_mills_time()
+                time_used = t2 - t1
+                wf.write(str(time_used) + '\n')
+            if query_key == 'q4':
+                # "q4": "SELECT COUNT(*) FROM testing WHERE int_col + int_col2 > 12345",
+                t1 = current_mills_time()
+                coll.find({'int_col': {"$gt": 5000}} ).limit(1000)
+                t2 = current_mills_time()
+                time_used = t2 - t1
+                wf.write(str(time_used) + '\n')
+            if query_key == 'q5':
+                # "q5": "SELECT * FROM testing WHERE int_col > 5000 ORDER BY word_col ASC LIMIT 1000",
+                t1 = current_mills_time()
+                coll.find({'int_col': {"$gt": bson.int64.Int64(5000)}}).sort([("word_col", pymongo.ASCENDING)]).limit(1000)
+                t2 = current_mills_time()
+                time_used = t2 - t1
+                wf.write(str(time_used) + '\n')
+            if query_key == 'q6':
+                # "q6": "SELECT * FROM testing WHERE word_col LIKE '%lim%' ORDER BY word_col DESC LIMIT 1000"
+                t1 = current_mills_time()
+                coll.find({"word_col":{"$regex": u"lim"}}).sort([("word_col", pymongo.ASCENDING)]).limit(1000)
+                t2 = current_mills_time()
+                time_used = t2 - t1
+                wf.write(str(time_used) + '\n')
+    def config(self):
+        config_file_locate = os.path.join(
+            self.write_dir, self.dbms.value + '_config.txt')
+        if self.dbms != DBMS.MONGODB:
+            with open(config_file_locate, 'wb') as wf:
+                conn = self._connect_db()
+                r = conn.cursor()
+                r.execute(self.query['config'])
+                for row in r:
+                    wf.write(str(row[0]) + ': ' + str(row[1]) + '\n')
+                r.close()
+                conn.close()
+        else:
+            pass
+            # pass for write config of mongo-db
+            # TODO: need to imp different way for mongo-db
 
     def summary(self):
-        wf = open(self.write_dir + '/' + self.pre + '_summary.txt', 'wb')
-        f = open(self.write_dir + '/' + self.pre + '_write.txt', 'r')
-        total_time = 0
-        total_line = 0
-        for line in f:
-            line = line.strip()
-            if line != '':
-                total_line = total_line + 1
-                total_time = total_time + float(line)
-        avg_time = total_time / total_line
-        wf.write('avg_write_time(ms/10000rows):{}\n'.format(
-            str(avg_time) + self.time_unit))
-        print('avg_write_time(ms/10000rows):{}'.format(
-            str(avg_time) + self.time_unit))
-        f.close()
-
-        for key in self.read_query:
-            f = open(self.write_dir + '/' + self.pre + '_' + key + '.txt', 'r')
-            total_time = 0
-            total_line = 0
-            for line in f:
-                line = line.strip()
-                if line != '':
-                    total_line = total_line + 1
-                    total_time = total_time + float(line)
-            avg_time = total_time/total_line
-            wf.write('avg_query_{}_time(ms):{}\n'.format(key, str(avg_time)))
-            print('avg_query_{}_time(ms):{}\n'.format(key, str(avg_time)))
-        wf.close()
+        summary_file = os.path.join(self.write_dir, self.dbms.value + '_summary.txt')
+        report_write_file = os.path.join(self.write_dir, self.dbms.value + '_write.txt')
+        with open( summary_file, 'wb') as wf:
+            with open(report_write_file, 'r') as f:
+                lines = [float(l)  for l in f.readlines()  if l.strip() != '']
+                avg_time = sum(lines) / len(lines)
+                wf.write('avg_write_time(ms/10000rows):{}\n'.format(str(avg_time)))
+                print('avg_write_time(ms/10000rows):{}'.format(str(avg_time)))
+            for key in self.read_query:
+                report_query_file = os.path.join(self.write_dir, self.dbms.value + '_' + key + '.txt')
+                with open(report_query_file, 'r') as f:
+                    lines = [float(l) for l in f.readlines() if l.strip() != '']
+                    avg_time = sum(lines) / len(lines)
+                    wf.write('avg_query_{}_time(ms):{}\n'.format(key, str(avg_time)))
+                    print('avg_query_{}_time(ms):{}\n'.format(key, str(avg_time)))
 
     def _connect(self):
-        if self.pre == 'pg':
+        if self.dbms == DBMS.POSTGRESQL:
             return psycopg2.connect(host=self.host, user=self.user, password=self.passwd, port=self.port)
-        else:
+        elif self.dbms == DBMS.MONGODB:
+            return MongoClient('mongodb://{}:{}'.format(self.host, self.port))
+        elif self.dbms == DBMS.MYSQL or self.dbms == DBMS.MARIADB:
             return MySQLdb.connect(host=self.host, user=self.user, passwd=self.passwd, port=self.port)
+        else:
+            raise("Cant connect to DB")
 
     def _connect_db(self):
-        if self.pre == 'pg':
+        if self.dbms == DBMS.POSTGRESQL:
             return psycopg2.connect(host=self.host, user=self.user, password=self.passwd, port=self.port)
-        else:
+        elif self.dbms == DBMS.MONGODB:
+            client = MongoClient(self.host, self.port)
+            return client['benchmark']
+        elif self.dbms == DBMS.MYSQL or self.dbms == DBMS.MARIADB:
             return MySQLdb.connect(host=self.host, user=self.user, passwd=self.passwd, db=self.db, port=self.port)
+        else:
+            raise("Cant connect to DB")
+
+
+def _benchmark(database):
+    logger.info("started benchmark for {}".format(database))
+    print("started benchmark for {}".format(database))
+    benchmark_db = database
+    dict_connection = CONNECTION_INIT[database.value]
+    benchmark = Benchmark(benchmark_db, dict_connection)
+    # benchmark.config()
+    # benchmark.write()
+    benchmark.read()
+    # benchmark.summary()
 
 
 if __name__ == "__main__":
-    for database in ['mysql', 'postgres', 'mariadb']:
-        logger.info("started benchmark for {}".format(database))
-        print("started benchmark for {}".format(database))
-        benchmark_db = database
-        if benchmark_db == 'postgres':
-            dict_connection = {
-                'host': '127.0.0.1',
-                'user': 'postgres',
-                'passwd': 'password',
-                'port': 5433
-            }
-        elif benchmark_db == 'mysql':
-            dict_connection = {
-                'host': '127.0.0.1',
-                'user': 'root',
-                'passwd': 'password',
-                'port': 3302
-            }
-        elif benchmark_db == 'mariadb':
-            dict_connection = {
-                'host': '127.0.0.1',
-                'user': 'root',
-                'passwd': 'password',
-                'port': 3307
-            }
-
-        benchmark = Benchmark(benchmark_db, dict_connection)
-        # benchmark.init() # comment benchmark.init() to disable initialize database + table
-        benchmark.config()
-        benchmark.write()
-        benchmark.read()
-        benchmark.summary()
+    _benchmark(DBMS.MONGODB)
+    # for database in ['mysql', 'postgres', 'mariadb', 'mongo']:
+    #     try:
+    #         _benchmark(database)
+    #     except Exception as e:
+    #         logger.error("Has error {}".format(e.message) )
--- word_list_generate.py
+++ word_list_generate.py
@@ -1,10 +1,10 @@
 import random
-
-f = open('word_list.txt', 'r')
+import json
 wordlist = []
-for line in f:
-    wordlist.append(line.strip())
 
+with open('word_list.txt', 'r') as f:
+    for line in f.readlines():
+        wordlist.append(line.strip())
 
 def generate_sql(table='testing', column=['word_col', 'int_col', 'int_col2'], col_type=['varchar', 'int', 'int'], line_ending="\n", rows=2048):
     line = ""
@@ -24,24 +24,20 @@ def generate_sql(table='testing', column=['word_col', 'int_col', 'int_col2'], co
             line = line + "), " + line_ending
     return line
 
-def generate_no_json(table='testing', column=['word_col', 'int_col', 'int_col2'], col_type=['varchar', 'int', 'int'], line_ending="\n", rows=2048):
-    line = ""
-    line = line + "insert into " + table + \
-        " (" + ", ".join(map(str, column)) + ") VALUES "
-    for i in range(rows):
-        line = line + "("
-        for v in col_type:
-            if v == 'varchar':
-                line = line + "'" + random_sentence() + "', "
-            elif v == 'int':
-                line = line + "" + str(random.randint(1, 10000)) + ", "
-        line = line.rstrip(', ')
-        if i == rows - 1:
-            line = line + ");" + line_ending
-        else:
-            line = line + "), " + line_ending
-    return line
 
+def generate_nosql(table='testing', column=['word_col', 'int_col', 'int_col2'], col_type=['varchar', 'int', 'int'], line_ending="\n", rows=2048):
+    lines = []
+    for i in range(rows):
+        line ={
+            'id': i,
+        }
+        for i, c in enumerate(column):
+            if col_type[i] == 'varchar':
+                line[c] = random_sentence().encode('utf-8')
+            elif col_type[i] == 'int':
+                line[c] = random.randint(1, 10000)
+        lines.append(line)
+    return lines
 
 
 def random_sentence(i=3):
@@ -51,15 +47,14 @@ def random_sentence(i=3):
         sentence = sentence + random.choice(wordlist) + ' '
     return sentence.strip()
 
+if __name__ == "__main__":
+    print("generating sql...")
+    with open('sql/insert.sql', 'wb') as outfile:
+        outfile.write(generate_sql(rows=10000).encode('utf-8'))
 
-print("generating sql")
-wf = open('sql/insert.sql', 'wb')
-wf.write(generate_sql(rows=10000).encode('utf-8'))
-wf.close()
+
+    print("generating nosql")
+    with open('nosql/data.json', 'wb') as outfile:
+        json.dump(generate_nosql(rows=10000), outfile)
 
 
-print("generating nosql")
-wf_no_sql = open('nosql/data.json', 'wb')
-wf_no_sql.write(generate_sql(rows=10000).encode('utf-8'))
-wf_no_sql.close()
-f.close
